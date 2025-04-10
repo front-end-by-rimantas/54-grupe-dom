d72612967f33caf73a6d99ce670bf946d1682031
Rimantas <belauzas@gmail.com>
2025-04-10 13:59:32 +0300

    Like

--- js/components/header.js
+++ js/components/header.js
@@ -14,6 +14,7 @@ export function header() {
         { text: 'Darzas', href: '/darzas/' },
         { text: 'Header', href: '/header/' },
         { text: 'Click', href: '/click/' },
+        { text: 'Like', href: '/like/' },
     ];
 
     let linksHTML = '';
